03f09f54591ac632d84425ac491837416888e965
Endrawan Andika Wicaksana <aendrawan@yahoo.co.id>
2023-08-12 18:06:28 +0700

    fix: fix menu navbar on mobile layout

--- personal-site/component/navbar.tsx
+++ personal-site/component/navbar.tsx
@@ -8,8 +8,10 @@ import {
   useColorModeValue,
   Stack,
   useColorMode,
+  useDisclosure,
+  IconButton,
 } from '@chakra-ui/react'
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 import Link from 'next/link'
 import { useCloserStore } from '@/store/closer';
 
@@ -49,15 +51,26 @@ const Links = [['Home', '/'], ['Blogs', '/articles'], ['Conference App (WebRTC)'
 
 export const Nav = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
             <Box>Endra's Web</Box>
-            {Links.map((link) => (
-            <NavLink link={link[1]} key={link[0]}>{link[0]}</NavLink>
-            ))}
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+              <NavLink link={link[1]} key={link[0]}>{link[0]}</NavLink>
+              ))}
+            </HStack>
           </HStack>
 
           <Flex alignItems={'center'}>
@@ -68,6 +81,16 @@ export const Nav = () => {
             </Stack>
           </Flex>
         </Flex>
+
+        {isOpen ?
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+              <NavLink link={link[1]} key={link[0]}>{link[0]}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        : <></>}
       </Box>
     </>
   )
